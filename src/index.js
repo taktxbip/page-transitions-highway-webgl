905fb62296f6cca558b5faae12d317c2aaed0c94
Evnomad <b.boy.bip@gmail.com>
2022-04-23 19:11:53 +0400

    highway part

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 'strict';
 
+import Highway from '@dogstudio/highway';
+import Fade from './js/modules/fade';
+
 import './scss/main.scss';
 import './js/assets';
 
 (function () {
     window.addEventListener('DOMContentLoaded', (e) => {
+        const H = new Highway.Core({
+            transitions: {
+                default: Fade
+            }
+        });
     });
 })();
