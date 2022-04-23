bf293c80c35ddc07d1989fcd6b1e69e6eba29b02
Evnomad <b.boy.bip@gmail.com>
2022-04-23 17:01:57 +0400

    clean

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 'strict';
 
-import Paginator from './js/modules/paginator';
-import './js/modules/animator';
-import './js/modules/nav';
-
-
 import './scss/main.scss';
 import './js/assets';
 
 (function () {
     window.addEventListener('DOMContentLoaded', (e) => {
-        new Paginator();
     });
 })();
